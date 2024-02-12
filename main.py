293da6f7587af01f9f31e50593b43b434d2f315b
aiserrock <aicserrock@gmail.com>
2024-02-12 23:35:58 +0300

    requirements.txt

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+import asyncio
+import configparser
+import requests
+import datetime
+from aiogram import Bot, Dispatcher, types
+from aiocron import crontab
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+config = configparser.ConfigParser()
+config.read('app.config')
+# Retrieve API keys from the configuration file
+TELEGRAM_BOT_TOKEN = config.get('API_KEYS', 'TELEGRAM_BOT_TOKEN')
+
+# Replace 'YOUR_BOT_TOKEN' and 'your_channel_id' with your actual values
+BOT_TOKEN = TELEGRAM_BOT_TOKEN
+# Replace YOUR_CHANNEL_ID
+CHANNEL_ID = '1002043368543'
+# Replace API
+BITCOIN_API_URL = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+async def send_bitcoin_price():
+    response = requests.get(BITCOIN_API_URL)
+    data = response.json()
+    bitcoin_price = data['bpi']['USD']['rate']
+    await bot.send_message(chat_id=CHANNEL_ID, text=f'Текущий курс биткоина: {bitcoin_price} USD')
 
 
-# Press the green button in the gutter to run the script.
+@dp.message_handler(commands=['start'])
+async def handle_start(message: types.Message):
+    await message.reply("Привет! Этот бот отправляет курс биткоина в ваш канал каждую минуту.")
+
+
+async def scheduler():
+    # Schedule the task using aiocron
+    crontab('*/1 * * * *', send_bitcoin_price, tz=datetime.datetime.now().astimezone().tzinfo)
+
+    while True:
+        await asyncio.sleep(60)  # Sleep for 60 seconds
+
+
+def main():
+    # Start the scheduler
+    asyncio.get_event_loop().run_until_complete(scheduler())
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()

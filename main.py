d9858db665c02495bdf11a0a498267b6ac392d01
Vladislav Zaikin <aicserrock@gmail.com>
2024-04-01 18:20:56 +0300

    rm coingeeko

--- main.py
+++ main.py
@@ -14,10 +14,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 CHANNEL_ID = os.getenv('CHANNEL_ID')
 LIVEPRICE_API_KEY = os.getenv('LIVEPRICE_API_KEY')
-
-# https://www.coingecko.com/api/documentation
-# /simple/price
-COINGECKO_API_URL = 'https://api.coingecko.com/api/v3/simple/price?ids=gram-2&vs_currencies=usd'
+MY_ID = os.getenv('MY_ID')
 
 # https://livecoinwatch.github.io/lcw-api-docs/?python#coinslist
 LIVEPRICE_API_URL = "https://api.livecoinwatch.com/coins/map"
@@ -27,9 +24,9 @@ dp = Dispatcher(bot)
 session = HTTP()
 
 
-async def send_bitcoin_price():
+async def send_price():
     payload = json.dumps({
-        "codes": ["BTC", "ETH", "KAS", "TONCOIN"],
+        "codes": ["BTC", "ETH", "KAS", "TONCOIN", "_GRAM", "CAS"],
         "currency": "USD",
         "sort": "age",
         "order": "descending",
@@ -40,16 +37,15 @@ async def send_bitcoin_price():
     }
 
     liveprice_response = requests.request("POST", LIVEPRICE_API_URL, headers=headers, data=payload)
-    coingecko_response = requests.get(COINGECKO_API_URL)
-    coingecko_data = coingecko_response.json()
     liveprice_data = liveprice_response.json()
 
     bitcoin_price = round(liveprice_data[0]['rate'])
     ethereum_price = round(liveprice_data[1]['rate'])
-    ton_price = round(liveprice_data[2]['rate'], 2)
-    kaspa_price = round(liveprice_data[3]['rate'], 3)
-    gram_price = round(coingecko_data['gram-2']['usd'], 4)
-    message = f'• BTC: ${bitcoin_price}\n• ETH: ${ethereum_price}\n• TON: ${ton_price}\n• KSP: ${kaspa_price}\n• GRAM: ${gram_price}'
+    ton_price = round(liveprice_data[2]['rate'], 1)
+    kaspa_price = round(liveprice_data[3]['rate'], 2)
+    gram_price = round(liveprice_data[4]['rate'], 3)
+    kaspa_classic_price = round(liveprice_data[5]['rate'], 4)
+    message = f'• BTC: ${bitcoin_price}\n• ETH: ${ethereum_price}\n• TON: ${ton_price}\n• KSP: ${kaspa_price}\n• GRAM: ${gram_price}\n• CAS: ${kaspa_classic_price}'
     await bot.send_message(chat_id=CHANNEL_ID, text=message)
     print(message)
     print('everything is working good! Next message will appear after 59 seconds')
@@ -58,6 +54,6 @@ async def send_bitcoin_price():
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     # Schedule the task to run every minute
-    aiocron.crontab('*/1 * * * *', func=send_bitcoin_price, loop=loop)
+    aiocron.crontab('*/1 * * * *', func=send_price, loop=loop)
     executor.start_polling(dp)
     loop.run_forever()

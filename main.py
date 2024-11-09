f2a576038c3aa77e3a822161e847e56278c8de7a
Vladislav Zaikin <aicserrock@gmail.com>
2024-11-09 12:19:59 +0300

    rm cache price

--- main.py
+++ main.py
@@ -45,45 +45,17 @@ async def _send_message_to_channel(message: str):
     print(message)
     print('everything is working good! Next message will appear after 59 seconds')
 
-
-async def _get_cache_prise() -> float:
-    result = requests.get(AKBARS_BEST_USD_PRICE, headers=HEADERS)
-    data = result.json()
-    cache_usd_price = round(data['branches'][0]['sellPrice'], 2)
-    return cache_usd_price
-
-
-async def _get_cbrf_prise() -> float:
-    cbrf = requests.get(CBRF, headers=HEADERS)
-    cbrf_data = cbrf.json()
-    cbrf_usd_price = round(cbrf_data['Valute']['USD']['Value'], 2)
-    return cbrf_usd_price
-
-
-async def _send_message_to_ls():
-    try:
-        cbrf = await _get_cbrf_prise()
-        cache = await _get_cache_prise()
-    except Exception:
-        cbrf = cache = 0.0
-    diff = round(abs(cbrf - cache), 2)
-    message = f'{cache} ₽ - Кеш в обменнике \n{cbrf} ₽ - ЦБ РФ\n{diff} ₽ - Дифф'
-    await bot.send_message(chat_id=MY_ID, text=message)
-    print(message)
-
-
 async def _iterate():
     try:
         liveprice_data = await _get_data()
         message = _make_currencies_price_message(liveprice_data)
         await _send_message_to_channel(message)
     except Exception:
-        await _send_message_to_channel('liveapi_error')
+        pass
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     aiocron.crontab('*/1 * * * *', func=_iterate, loop=loop)
-    aiocron.crontab('*/1 * * * *', func=_send_message_to_ls, loop=loop)
     executor.start_polling(dp)
     loop.run_forever()
